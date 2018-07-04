c1fb98a0b2e9be6def7aba1a3787ae305e4c22e2
VictorMmendes.VM <VictorMmendes.VM@gmail.com>
2018-07-04 16:37:52 -0300

    get

--- rest.php
+++ rest.php
@@ -17,17 +17,46 @@
 		$stmt = $conn->prepare($sql);
 		$stmt->execute();
 
-		// $animais = $stmt->fetchAll();
-		// foreach ($animais as $animal) {
-		// 	echo $animal['id'] . "<br>";
-		// 	echo $animal['name'] . "<br>";
-		// 	echo $animal['species'] . "<br>";
-		// 	echo $animal['breed'] . "<br>";
-		// 	echo $animal['weight'] . "<br>";
-		// 	echo $animal['birthdate'] . "<br>";
-		// 	echo $animal['size'] . "<br><br>";
-		// }
 		echo json_encode($stmt->fetchAll());
+	});
+
+	$app->get('/insert.json/:description&:repeats&:weight', function($description, $repeats, $weight) {
+
+			$sql = "INSERT INTO exercicio (description, repeats, weight) values('$description', '$repeats', $weight)";
+			$conn = getConn();
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+
+			echo json_encode("OK");
+	});
+
+	$app->post('/exercicios.json', function() use($app)
+	{
+		$dadoJson = $app->request()->getBody();
+		echo parse_str($dadoJson);
+		// echo $app;
+		// $description=$dadoJson[0]->description;
+		// $repeats=$dadoJson[0]->repeats;
+		// $weight=$dadoJson[0]->weight;
+
+		// $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+		// $txt = "" . $dadoJson;
+		// fwrite($myfile, $txt);
+		// fclose($myfile);
+
+		// $sql = "INSERT INTO exercicio (description, repeats, weight) values('oii', '$dadoJson', 12)";
+		// $conn = getConn();
+		// $stmt = $conn->prepare($sql);
+		// $stmt->execute();
+
+		// $id = $conn->lastInsertId();
+		// $data = date('Y-m-d');
+		//
+		// $sql = "INSERT INTO modificacao (progressdate, id_exercise, weight) values('$data', $id, '$weight')";
+		// $stmt = $conn->prepare($sql);
+		// $stmt->execute();
+
+		echo json_encode("OK");
 	});
 
 	$app->get('/modificacoes.json/:id', function($id) {
